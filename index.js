c5cf72638572ca0de7cd6c023ee5e4f500c7da02
Raeyaan <raeyaanshahid788@gmail.com>
2024-04-20 06:45:26 +0200

    vercel 7

--- index.js
+++ index.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import fetch from 'node-fetch';
 import dotenv from 'dotenv';
+import  path from 'path';
+import { fileURLToPath } from 'url';
 
 dotenv.config();
 
 const app = express();
 
-app.set("views", process.cwd() + "/views");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
+
+app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
-app.use(express.static('lib'));
-app.use(express.static('images'));
-app.use(express.static('views'));
+app.use(express.static(__dirname +'lib'));
+app.use(express.static(__dirname +'images'));
+app.use(express.static(__dirname +'views'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
